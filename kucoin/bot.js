09ec6d8494e47d7a707fef2ff528a06e4f0ff74f
egeominotti <egeominotti@gmail.com>
2021-11-29 04:06:47 +0100

    -

--- kucoin/bot.js
+++ kucoin/bot.js
@@ -1,30 +1,17 @@
-const Kucoin = require("kucoin-websocket-api")
-const api = require('kucoin-node-api')
+let done = true
 
-const config = {
-  apiKey: 'xXXXXXXXXXXXXXXXXXXXXxxxxxxxxxxxxxxxxxxxxxXXX',
-  secretKey: 'xxxxxxxxXXXXXXXXXXXXXxxXXXXXXXXXXXXXxxxXXX',
-  passphrase: 'xxxxxx',
-  environment: 'test'
-}
-
-api.init(config)
-const client = new Kucoin()
-
-console.log(api.getAllTickers(function(response){
-  console.log(response)
-}));
-// Maximum 100 Symbol / Connection!
-const symbols = ["BTC-USDT", "ETH-BTC"]
-
-// Public streaming websocket for the orderbook of the provide symbol(s)
-api.initSocket({topic: "orderbook", symbols: ['KCS-BTC']}, (msg) => {
-  let data = JSON.parse(msg)
-  console.log(data)
+const isItDoneYet = new Promise((resolve, reject) => {
+  if (done) {
+    const workDone = 'Here is the thing I built'
+    resolve(workDone)
+  } else {
+    const why = 'Still working on something else'
+    reject(why)
+  }
 })
 
-// Private streaming websocket for account balances
-api.initSocket({topic: "balances"}, (msg) => {
-  let data = JSON.parse(msg)
-  console.log(data)
+isItDoneYet.then(ok => {
+  console.log("ok");
+}).catch(err => {
+  console.log(err)
 })
