03bce1fec1e0ba0516d7a36dd2f5a6f4f35ae600
egeominotti <egeominotti@gmail.com>
2021-11-29 18:31:04 +0100

    - fix

--- models/logger.js
+++ models/logger.js
@@ -2,62 +2,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema
 
 const loggerSchema = new Schema({
-    type: {
-        type: String,
-        required: false
-    },
-    symbol: {
-        type: String,
-        required: false
-    },
-    interval: {
-        type: String,
-        required: false
-    },
-    entrydata: {
-        type: String,
-        required: false
-    },
-    takeprofitvalue: {
-        type: String,
-        required: false
-    },
-    stoplossvalue: {
-        type: String,
-        required: false
-    },
-    takeprofitpercentage: {
-        type: String,
-        required: false
-    },
-    takeprofitdate: {
-        type: Date,
-        required: false
-    },
-    stoplosspercentage: {
-        type: String,
-        required: false
-    },
-    stoplossdate: {
-        type: Date,
-        required: false
-    },
-    hh: {
-        type: String,
-        required: false
-    },
-    ll: {
-        type: String,
-        required: false
-    },
-    lh: {
-        type: String,
-        required: false
-    },
-    hl: {
-        type: String,
-        required: false
-    },
+    type: {type: String, required: false},
+    symbol: {type: String, required: false},
+    interval: {type: String, required: false},
+    entrydata: {type: String, required: false},
+    takeprofitvalue: {type: String, required: false},
+    stoplossvalue: {type: String, required: false},
+    takeprofitpercentage: {type: String, required: false},
+    takeprofitdate: {type: Date, required: false},
+    stoplosspercentage: {type: String, required: false},
+    stoplossdate: {type: Date, required: false},
+    hh: {type: String, required: false},
+    ll: {type: String, required: false},
+    lh: {type: String, required: false},
+    hl: {type: String, required: false},
 });
 
 module.exports = mongoose.model('Logger', loggerSchema)
