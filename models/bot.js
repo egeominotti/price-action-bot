0d34ccf391d7ed958bd8ca6f542612a9bed7284c
egeominotti <egeominotti@gmail.com>
2021-11-29 05:27:01 +0100

    -

--- models/bot.js
+++ models/bot.js
@@ -1,3 +1,12 @@
+// https://www.youtube.com/watch?v=bxsemcrY4gQ
+/*
+username = doadmin
+password = 69f7g4dYoP8I23p1 hide
+host = db-mongodb-ams3-34537-4eea409b.mongo.ondigitalocean.com
+port = 27017
+database = admin
+protocol = mongodb+srv
+ */
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/test');
 
