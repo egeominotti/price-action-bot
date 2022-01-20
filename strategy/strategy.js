38f37778557d5aeeaec92ccaa2cd5aca99631406
egeominotti <egeominotti@gmail.com>
2022-01-20 01:36:35 +0100

    - test

--- strategy/strategy.js
+++ strategy/strategy.js
@@ -22,7 +22,6 @@ function strategyBreakout(symbol, interval, close, record) {
     let lh = record['lh']
     let hl = record['hl']
 
-
     let takeprofit = (close - ll) * 2 + close;
     let stoploss = ll * 0.99;
 
