ae6f2fc3d9a6da85a981e2ce0c1e6b1881d4cde1
egeominotti <egeominotti@gmail.com>
2022-01-29 18:13:44 +0100

    - fix bot entry - refactoring - fix unlock pair for time frame

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 docker build -t egeominotti/bot-price-action .
 docker push egeominotti/bot-price-action:latest
 ssh root@49.12.78.119 /home/deploy.sh
-ssh root@49.12.78.119 docker ps
-ssh root@49.12.78.119
+ssh root@49.12.78.119 docker logs bot -f
+
