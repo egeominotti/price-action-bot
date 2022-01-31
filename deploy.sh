8b525dd43dcd947142c0b34c1eab982f727c9781
kernel_void <egeominotti@gmail.com>
2022-02-01 00:02:44 +0100

    - docker

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,5 @@ docker push egeominotti/bot-price-action:latest
 docker build -t egeominotti/bot-price-action-frontend:latest ./frontend
 docker push egeominotti/bot-price-action-frontend:latest
 
-#
-#ssh root@49.12.78.119 /home/deploy.sh
-#ssh root@49.12.78.119 docker logs bot -f
+ssh root@49.12.78.119 /home/deploy.sh
+ssh root@49.12.78.119 docker logs bot -f
