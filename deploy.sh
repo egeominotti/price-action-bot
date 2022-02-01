3921812316e655477fb0e69352b1bc0fccc83f57
kernel_void <egeominotti@gmail.com>
2022-02-01 01:14:17 +0100

    - docker

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,13 @@
 
 docker system prune -af
 
-
 docker image build -t egeominotti/bot-price-action:latest .
 docker push egeominotti/bot-price-action:latest
 
-#docker build -t egeominotti/bot-price-action-frontend:latest ./frontend
-#docker push egeominotti/bot-price-action-frontend:latest
+docker build -t egeominotti/bot-price-action-frontend:latest ./frontend
+docker push egeominotti/bot-price-action-frontend:latest
 
-ssh root@49.12.78.119 /home/deploy.sh
+ssh root@49.12.78.119 /home/erase_engine.sh
+ssh root@49.12.78.119 /home/deploy_backend.sh
+ssh root@49.12.78.119 /home/deploy_frontend.sh
 ssh root@49.12.78.119 docker logs bot -f
