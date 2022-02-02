ff2128b66c661ee43e3acaac94bff63a20238997
egeominotti <egeominotti@gmail.com>
2022-02-02 13:37:38 +0100

    - fixed ema from cache memory

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,4 @@ COPY package*.json ./
 RUN npm install
 COPY . .
 EXPOSE 3000
-CMD node engine/bot.js
+CMD node engine/bot_v2.js
--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,13 @@
 
 docker system prune -af
 
-docker image build -t egeominotti/bot-price-action:latest .
-docker push egeominotti/bot-price-action:latest
+docker image build -t egeominotti/bot-price-action-v2:latest .
+docker push egeominotti/bot-price-action-v2:latest
 
 docker build -t egeominotti/bot-price-action-frontend:latest ./frontend
 docker push egeominotti/bot-price-action-frontend:latest
 
-ssh root@49.12.78.119 /home/erase_engine.sh
-ssh root@49.12.78.119 /home/deploy_backend.sh
-ssh root@49.12.78.119 /home/deploy_frontend.sh
-ssh root@49.12.78.119 docker logs bot -f
+ssh root@188.34.189.183 /home/erase_engine.sh
+ssh root@188.34.189.183 /home/deploy_backend.sh
+ssh root@188.34.189.183 /home/deploy_frontend.sh
+ssh root@188.34.189.183 docker logs bot -f
