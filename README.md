22f23b13355b149fecf704554cd65c50924c5d9c
egeominotti <egeominotti@gmail.com>
2022-08-17 13:32:03 +0200

    - fix fix

--- README.md
+++ README.md
@@ -3,22 +3,3 @@
     ## Init bot iterate all time frame and spawn for each bot
     - node engine/bot.js    
 
-# Docker Compile DockerFile
-
-    - Enable watchtower
-         
-        docker run --restart unless-stopped -d  \
-            --name watchtower \
-            -e REPO_USER=egeominotti \
-            -e REPO_PASS=cevfag12 \
-            -v /var/run/docker.sock:/var/run/docker.sock \
-            containrrr/watchtower --interval 300 -c --include-restarting
-
-    - Run bot
-
-        docker run --restart unless-stopped -d  egeominotti/bot-price-action:latest
-
-    - Compiling and Push
-
-        docker build -t egeominotti/bot-price-action .
-        docker push egeominotti/bot-price-action:latest
--- utility/telegram.js
+++ utility/telegram.js
@@ -1,34 +1,20 @@
 const axios = require("axios");
 require('dotenv').config();
 
-let bot_token = ''
-let bot_chat_id = ''
 
-if (process.env.DEBUG === 'false') {
-    bot_token = '1889367095:AAGS13rjA6xWAGvcUTOy1W1vUZvPnNxcDaw'
-    bot_chat_id = '-558016221'
-} else {
-    bot_token = '2136128892:AAFOQs6Qri5eqZKIeEovq8wthAxPYDjkBkY'
-    bot_chat_id = '-699520069'
-}
+const telegram = 'https://api.telegram.org/bot'
+    + process.env.BOT_TOEKN + '/sendMessage?chat_id='
+    + process.env.CHAT_ID + '&parse_mode=Markdown&text='
 
-/**
- *
- * @param text
- */
+
 function sendMessage(text) {
 
-    const send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chat_id + '&parse_mode=Markdown&text=' + text
-
-    axios.get(send_text)
+    axios.get(telegram + text)
         .then(function (response) {
         })
         .catch(function (error) {
             console.log(error)
         })
-        .then(function () {
-        });
-
 }
 
 module.exports = {
